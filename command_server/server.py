72d2cab86c4512e74d2f42f45e113bbd77c3c8ff
Tony Boldurian <tony.boldurian@pm.me>
2023-02-01 17:42:46 -0500

    Set shebang and create comment headers

--- command_server/server.py
+++ command_server/server.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+#-------------------------------------------------------------------
+#  File: commmand_server/server.py
+#  Summary: Python script that creates a HTTP server that listens for specific custom HTTP GET headers.
+#           Translates these headers into ROS2 instructions and/or scripts to be run on the middle-man hardware.
+#  Functions:
+#  run_server(server_class, handler_class, port)
+#-------------------------------------------------------------------
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging, os
 
@@ -21,7 +29,18 @@ class Server(BaseHTTPRequestHandler):
                 print("Unknown SIGMAP-CMD")
 
         self._set_reponse()
-        
+
+#-------------------------------------------------------------------
+#  Function: run_server
+#  Summary: Creates the HTTP server using the implementation class created above
+#  Params: server_class
+#          The server will be running as HTTPServer
+#          handler_class
+#          HTTPServer will run with handler implementation created above called Server
+#          port
+#          Sets port the server will run on
+#  Returns: Description
+#-------------------------------------------------------------------
 def run_server(server_class=HTTPServer, handler_class=Server, port=8080):
     logging.basicConfig(level=logging.INFO)
     address = ('', port)
