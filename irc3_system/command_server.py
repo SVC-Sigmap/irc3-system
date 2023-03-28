4ff59171bd7179d36894591f177495fea69be753
Cameron Cox <camjcox19@gmail.com>
2023-03-28 14:28:51 -0400

    Resolving issues including ros2

--- irc3_system/command_server.py
+++ irc3_system/command_server.py
@@ -51,7 +51,7 @@ def webhook():
                     
                     print('Teleop_Keyboard Command Recieved!')
                     try:
-                        teleop_keyboard = subprocess.Popen([ros2_path, 'ros2', 'run', 'teleop_twist_keyboard', 'teleop_twist_keyboard','{}'])
+                        teleop_keyboard = subprocess.Popen([ros2_path, 'run', 'teleop_twist_keyboard', 'teleop_twist_keyboard','{}'])
                         processes.append(teleop_keyboard)
                     except KeyboardInterrupt:
                         pass
@@ -63,7 +63,7 @@ def webhook():
                     
                     print('Teleop_Joystick Command Recieved!')
                     try:
-                        teleop_joystick = subprocess.Popen([ros2_path, 'ros2', 'launch', 'create3_teleop', 'teleop_joystick_launch.py', 'joy_dev:=/dev/input/js1','{}'])
+                        teleop_joystick = subprocess.Popen([ros2_path, 'launch', 'create3_teleop', 'teleop_joystick_launch.py', 'joy_dev:=/dev/input/js1','{}'])
                         processes.append(teleop_joystick)
                     except KeyboardInterrupt:
                         pass
